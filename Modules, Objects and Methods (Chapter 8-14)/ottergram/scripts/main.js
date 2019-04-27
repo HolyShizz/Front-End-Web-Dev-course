cafac822de5dbbd5af0a91715ac98844ced4e761
Holyshizz <filippsoltys@gmail.com>
2019-04-27 16:15:44 +0300

    Second Chapter start

--- Modules, Objects and Methods (Chapter 8-14)/ottergram/scripts/main.js
+++ Modules, Objects and Methods (Chapter 8-14)/ottergram/scripts/main.js
@@ -1,6 +1,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
 
 
 function setDetails(imageUrl, titleText) {
@@ -32,6 +36,7 @@ function addThumbClickHandler(thumb) {
     thumb.addEventListener('click', function (event) {
         event.preventDefault();
         setDetailsFromThumb(thumb);
+        showDetails();
     })
 };
 
@@ -42,16 +47,49 @@ function getThumbnailsArray() {
     return thumbnailsArray;
 }
 
+// Chapter 1.7 Animation with CSS
+
+//Function for hiding detail-image by adding CSS class to the body
+
+function hideDetails() {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    'use strict';
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function(){
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+//Event handler for keyboard interaction("keyup")
+
+function addKeyPressHandler() {
+    'use strict';
+    document.body.addEventListener('keyup', function (event) {
+        event.preventDefault();
+        console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hideDetails()
+        }
+    })
+};
+
 function initializeEvents() {
     'use strict';
     var thumbnails = getThumbnailsArray();
     thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
 }
 
 initializeEvents();
 
 
-/*Gold Challenge
+/* Chapter 1.6 Gold Challenge
 Write a function that changes the [data-image-url] of a random thumbnail so 
 that detail image no longer matches the thumbnail*/
 
@@ -66,7 +104,7 @@ function addRandomThumbnailImgSrc() {
 }
 
 
-/* Gold Challenge
+/* Chapter 1.6 Gold Challenge
 For an extra challenge, write a function that resets your thumbnails to their orirginal state*/
 
 //Function for reseting all data-image-url attribute values 
@@ -112,7 +150,7 @@ function addResetClickHandler(thumb) {
     })
 };
 
-//Initializing handlers for buttons
+//Initializing handlers on mix and reset buttons
 function initializeButtons() {
     'use strict';
     createThumbSrcArray(THUMBNAIL_LINK_SELECTOR_Array);
@@ -123,3 +161,4 @@ function initializeButtons() {
 }
 
 initializeButtons();
+
